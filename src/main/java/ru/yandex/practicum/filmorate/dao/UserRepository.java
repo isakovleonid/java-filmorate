4fe71f551a3a250ff7635a99cf21b785ead77d49
Isakov Leonid <isakov.leonid@gmail.com>
2025-04-11 12:03:54 +0300

    Спринт 12 Финальное задание
    Доработка согласно codestyle

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class UserRepository extends BaseRepository{
+public class UserRepository extends BaseRepository {
     private static final String INSERT_QUERY = "insert into Users(name, login, birthday, email) " +
             "values (?, ?, ?, ?)";
 
