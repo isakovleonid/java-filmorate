235913cda43a3100c581a2f9f8ed1da7ab32f29c
Isakov Leonid <isakov.leonid@gmail.com>
2025-04-24 16:49:31 +0300

    Спринт 12 Финальное задание
    1) Использование интерфейса Storage вместо repository
    2) FilmDTOMapper - получение genresDict делаем из БД за раз d map

--- src/main/java/ru/yandex/practicum/filmorate/dto/FilmDTOMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/FilmDTOMapper.java
@@ -2,11 +2,11 @@ package ru.yandex.practicum.filmorate.dto;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.dao.GenreDictRepository;
-import ru.yandex.practicum.filmorate.dao.MPARatingFilmRepository;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.GenreDict;
 import ru.yandex.practicum.filmorate.model.MPARatingFilm;
+import ru.yandex.practicum.filmorate.storage.GenreDictStorage;
+import ru.yandex.practicum.filmorate.storage.MPARatingFilmStorage;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -14,8 +14,8 @@ import java.util.stream.Collectors;
 @Component
 @RequiredArgsConstructor
 public class FilmDTOMapper implements DTOMapper<FilmDTO, Film> {
-    private final MPARatingFilmRepository mpaRatingFilmRepository;
-    private final GenreDictRepository genreDictRepository;
+    private final MPARatingFilmStorage mpaRatingFilmStorage;
+    private final GenreDictStorage genreDictStorage;
 
     @Override
     public Film fromDTO(FilmDTO filmDTO) {
@@ -26,6 +26,7 @@ public class FilmDTOMapper implements DTOMapper<FilmDTO, Film> {
         film.setReleaseDate(filmDTO.getReleaseDate());
         film.setDuration(filmDTO.getDuration());
         film.setMpa(filmDTO.getMpa().getId());
+
         film.setGenres(filmDTO.getGenres().stream()
                 .map(GenreDict::getId)
                 .collect(Collectors.toSet())
@@ -44,13 +45,17 @@ public class FilmDTOMapper implements DTOMapper<FilmDTO, Film> {
         filmDTO.setDuration(film.getDuration());
 
         if (film.getMpa() != null) {
-            MPARatingFilm mpaRatingFilm = mpaRatingFilmRepository.getById(film.getMpa());
+            MPARatingFilm mpaRatingFilm = mpaRatingFilmStorage.getById(film.getMpa());
             filmDTO.setMpa(mpaRatingFilm);
         }
 
+        Map<Long, GenreDict> mapGenreDict = new HashMap<>();
+        mapGenreDict = genreDictStorage.getAll().stream()
+                .collect(Collectors.toMap(GenreDict::getId, genre -> genre));
+
         Set<GenreDict> genreDictSet = film.getGenres().stream()
                         .filter(Objects::nonNull)
-                        .map(genreDictRepository::getById)
+                        .map(mapGenreDict::get)
                         .collect(Collectors.toCollection(LinkedHashSet::new));
         filmDTO.setGenres(genreDictSet);
 
